4d8f5864337628e23472f424513b00769e3e96a9
ghanshyamsahutech01 <ghanshyamsahutech01@gmail.com>
2026-04-01 23:47:42 +0530

    Added CheckArmstrong.cpp and CheckPrimeNo.cpp files

---
+++ CPP_OOPs/CheckPrimeNo.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+//#include <conio.h>
+using namespace std; //No use in Turbo
+class PrimeOrNot
+{
+    int num, isPrime;
+    public:
+    void getdata();
+    void showPrimeOrNot();
+};
+void PrimeOrNot::getdata(){
+    cout << "Enter any number: ";
+    cin >> num;
+}
+void PrimeOrNot::showPrimeOrNot(){
+    if(num<=0){
+        cout << "Please enter a positive number!" << endl;
+    }
+    else{
+        if(num==1){
+            cout << "1 is neither Prime nor Composite." << endl;
+        }
+        else{
+            isPrime=1;
+            for(int i=2;i<=num/2;i++){
+                if(num%i==0){
+                    isPrime=0;
+                    break;
+                }
+            }
+            if(isPrime)
+                cout << num << " is a Prime number." << endl;
+            else
+                cout << num << " is a Composite number." << endl;
+        }        
+    }                 
+}
+int main(){ //Use void main in Turbo
+    //clrscr();
+    PrimeOrNot p1;
+    p1.getdata();
+    p1.showPrimeOrNot();
+    //getch();
+    return 0;
+}    
