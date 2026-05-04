5094616dbd9e8cb32ad7423371b6e1e2992e01ab
ghanshyamsahutech01 <ghanshyamsahutech01@gmail.com>
2026-05-04 19:38:11 +0530

    Added new C program of DS

---
+++ Data_Structure/Transpose.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+//#include<conio.h>
+int main(){
+    int m,n;
+    int i, j;
+    int a[10][10];
+    printf("Enter row and column for Matrix = ");
+    scanf("%d%d",&m,&n);
+    printf("\nEntery for Matrix:\n");
+    for(i=0;i<m;i++){
+        printf("Enter %d row = ",i+1);
+        for(j=0;j<n;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("\nOriginal Matrix:\n");
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\nTranspose Matrix:\n");
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            printf("%d ",a[j][i]);
+        }
+        printf("\n");
+    }
+    //getch();
+    return 0;
+}    
