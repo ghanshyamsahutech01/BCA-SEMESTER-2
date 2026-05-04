c4c6446ebee66b3d305e7e39aa906ca2479aea03
ghanshyamsahutech01 <ghanshyamsahutech01@gmail.com>
2026-05-04 22:30:08 +0530

    Fix Transpose.c file

--- Data_Structure/Transpose.c
+++ Data_Structure/Transpose.c
@@ -22,9 +22,9 @@ int main(){
         printf("\n");
     }
     printf("\nTranspose Matrix:\n");
-    for(i=0;i<m;i++){
-        for(j=0;j<n;j++){
-            printf("%d ",a[j][i]);
+    for(j=0;j<n;j++){
+        for(i=0;i<m;i++){
+            printf("%d ",a[i][j]);
         }
         printf("\n");
     }
