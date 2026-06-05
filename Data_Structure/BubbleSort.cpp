477b27cdec69c229b1ae2604183356e501e745b3
ghanshyamsahutech01 <ghanshyamsahutech01@gmail.com>
2026-06-05 21:33:28 +0530

    Added CPP program Selection Sort and BubbleSort in DS

---
+++ Data_Structure/BubbleSort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+//#include<conio.h>
+using namespace std;
+// Function Prototype
+void bubbleSort(int arr[], int n);
+int main()
+{
+    //clrscr();
+    int n,arr[50],i;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    cout << "Enter elements: ";
+    for(i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    bubbleSort(arr, n);
+    cout << "Sorted Array: ";
+    for(i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    //getch();
+    return 0;
+}
+// Function Definition
+void bubbleSort(int arr[], int n)
+{
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = 0; j < n - 1 - i; j++)
+        {
+            if(arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
---
+++ Data_Structure/SelectionSort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+// Function Prototype
+void swapNumbers(int &a, int &b);
+int main()
+{
+    int i,j,n,arr[50],min;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    cout << "Enter elements: ";
+    for( i = 0; i < n; i++)
+        cin >> arr[i];
+    for( i = 0; i < n - 1; i++)
+    {
+        min = i;
+        for( j = i + 1; j < n; j++)
+        {
+            if(arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+        if(min != i) {
+            swapNumbers(arr[i], arr[min]);
+        }
+    }
+    cout << "Sorted Array: ";
+    for(i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
+// Function Definition
+void swapNumbers(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
